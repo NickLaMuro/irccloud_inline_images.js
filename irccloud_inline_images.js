ef1198370e595dd3e5d191f697e4288fa01c270f
Nick LaMuro <nicklamuro@gmail.com>
2012-07-14 15:17:11 -0500

    Adds #convert_link_to_image

--- irccloud_inline_images.js
+++ irccloud_inline_images.js
@@ -1,11 +1,17 @@
 (function() {
+
+  function convert_link_to_image(link) {
+    var href_url = $(link).attr("href")
+    var image_tag = '<img src="' + href_url + '"></img>';
+    $(link).replaceWith(image_tag);
+  }
+
   $('td#maincell').delegate("table.buffer.channel:visible div.log", "DOMNodeInserted", function(e) {
     //console.log(e.target);
     //console.log( $("span.message a", e.target).filter(function() { return /.*(\.jpg|\.png|\.gif)$/.test($(this).attr("href")) }) );
     $("span.message a", e.target).filter(function() { return /.*(\.jpg|\.png|\.gif)$/.test($(this).attr("href")) }).each(function(index) {
-      var href_url = $(this).attr("href")
-      var image_tag = '<img src="' + href_url + '"></img>';
-      $(this).replaceWith(image_tag);
+      convert_link_to_image(this)
     });
   });
+
 })();
