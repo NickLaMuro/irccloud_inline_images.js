ed2ec986aa99f55cb36b7099f5b34447d6518d52
Nick LaMuro <nicklamuro@gmail.com>
2012-07-23 23:32:37 -0500

    Attempt number 3 for applying a stylesheet to the DOM

--- irccloud_inline_images.js
+++ irccloud_inline_images.js
@@ -5,7 +5,7 @@
   cssCode.href = 'https://raw.github.com/NickLaMuro/irccloud_inline_images.js/master/inline_images.css';
   cssCode.type = 'text/css';
   cssCode.media = 'all';
-  document.head.appendChild(cssCode);
+  document.getElementByTagName('head')[0].appendChild(cssCode);
 
   function convert_link_to_image(link) {
     var href_url = $(link).attr("href")
