67c77732919bd63a4babb0b051950baf352face5
Nick LaMuro <nicklamuro@gmail.com>
2012-07-24 09:23:05 -0500

    Adds active conditional so this can be called multiple times safely

--- irccloud_inline_images.js
+++ irccloud_inline_images.js
@@ -1,12 +1,24 @@
 (function() {
 
-  var cssCode = document.createElement('link');
-  cssCode.rel = 'stylesheet';
-  cssCode.href = 'http://nicklamuro.github.com/irccloud_inline_images.js/stylesheets/inline_images.css';
-  cssCode.type = 'text/css';
-  cssCode.media = 'all';
-  document.getElementsByTagName('head')[0].appendChild(cssCode);
+  if(!irccloudInlineImagesActive) {
+    console.log("This should only be called once");
 
+    var cssCode = document.createElement('link');
+    cssCode.rel = 'stylesheet';
+    cssCode.href = 'http://nicklamuro.github.com/irccloud_inline_images.js/stylesheets/inline_images.css';
+    cssCode.type = 'text/css';
+    cssCode.media = 'all';
+    document.getElementsByTagName('head')[0].appendChild(cssCode);
+
+    $('td#maincell').delegate("table.buffer.channel:visible div.log", "DOMNodeInserted", function(e) {
+      $("span.message a", e.target).filter(function() { return /.*(\.jpg|\.png|\.gif)$/.test($(this).attr("href")) }).each(function(index) {
+        convert_link_to_image(this)
+      });
+    });
+
+  }
+
+  console.log("This should be called every time");
   function convert_link_to_image(link) {
     var href_url = $(link).attr("href")
     var image_tag = '<img src="' + href_url + '" class="inline_image"/>';
@@ -17,10 +29,6 @@
     convert_link_to_image(this)
   });
 
-  $('td#maincell').delegate("table.buffer.channel:visible div.log", "DOMNodeInserted", function(e) {
-    $("span.message a", e.target).filter(function() { return /.*(\.jpg|\.png|\.gif)$/.test($(this).attr("href")) }).each(function(index) {
-      convert_link_to_image(this)
-    });
-  });
+  window.irccloudInlineImagesActive = true;
 
 })();
